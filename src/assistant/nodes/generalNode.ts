f8d32a90052baaa03d0f44bacb004c823c331ad6
fesmofet <borees2@gmail.com>
2024-10-07 16:59:32 +0300

    add site info

--- src/assistant/nodes/generalNode.ts
+++ src/assistant/nodes/generalNode.ts
@@ -10,6 +10,25 @@ import {
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { GraphState } from '../index';
 import { getWeaviateStore } from '../store/weaviateStore';
+import { configService } from '../../config';
+import { createFetchRequest } from '../helpers/createFetchRequest';
+
+type sitesDescriptionType = {
+  result: string;
+};
+const getSiteDescription = async (host: string): Promise<string> => {
+  configService.getAppHost();
+  const url = `https://${configService.getAppHost()}/api/sites/description`;
+  const response = await createFetchRequest({
+    api: { method: 'GET', url },
+    params: {},
+    accessHost: host,
+  });
+
+  const { result } = response as sitesDescriptionType;
+
+  return result;
+};
 
 export const generalNode = async (
   state: GraphState,
@@ -17,6 +36,7 @@ export const generalNode = async (
   const { llm, query, chatHistory, nextRepresentative, host } = state;
 
   const vectorStore = await getWeaviateStore(nextRepresentative);
+  const siteDescription = await getSiteDescription(host);
 
   const contextualizeQPrompt = ChatPromptTemplate.fromMessages([
     [
@@ -28,6 +48,7 @@ export const generalNode = async (
   ]);
 
   const qaSystemPrompt = `You are an assistant for question-answering tasks on ${host}
+    ${siteDescription ? `short description: ${siteDescription}` : ''}
     Use the following pieces of retrieved context to answer the question.
     Whenever possible, accompany your answers with links and images (![image]) to relevant articles or lessons. 
     Keep the answer concise. Don't use "AI:" in answers.
