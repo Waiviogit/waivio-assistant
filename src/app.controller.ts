c19fde8788b09ba3c690276c4ddd248c38572bbe
fesmofet <borees2@gmail.com>
2024-10-18 15:08:02 +0300

    test cleangirl

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ import { MessageInDto } from './dto/message-in.dto';
 import { MessageOutDto } from './dto/message-out.dto';
 import { HistoryOutDto } from './dto/history-out.dto';
 import { AppControllerDoc } from './app.controller.doc';
+import { cleanNode } from './assistant/nodes/cleangirl';
 
 @Controller()
 @AppControllerDoc.main()
@@ -29,5 +30,14 @@ export class AppController {
     id: string,
   ): Promise<HistoryOutDto> {
     return this.appService.getHistory(id);
+  }
+
+  @Post('clean')
+  cleanMessage(
+    @CustomHeaders(new HostPipe())
+    host: string,
+    @Body() body: MessageInDto,
+  ) {
+    return cleanNode(body.query);
   }
 }
